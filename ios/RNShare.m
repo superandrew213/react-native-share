cd1932a9403eb9783c073fbaf03ff4b5317ff5f0
Andrew Shini <superandrew213@users.noreply.github.com>
2017-07-03 18:54:11 +1000

    Use UIActivityViewController to exclude activity types

--- ios/RNShare.m
+++ ios/RNShare.m
@@ -15,26 +15,60 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(open:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
 {
     NSString *shareFile = [RCTConvert NSString:options[@"share_file"]];
+    
     // Checks if http or https
     BOOL isRemote = [NSURL URLWithString:shareFile].scheme;
+    // Check if limited
+    BOOL isLimited = [RCTConvert BOOL:options[@"limited"]];
     
+    NSURL *fileToShare;
     if (isRemote) {
+        // Download file first
         NSURL *fileUrl = [NSURL URLWithString:shareFile];
-        NSURL *downloadedFileUrl = [self downloadFile:fileUrl];
-        if (downloadedFileUrl) {
-            [self displayDocument:downloadedFileUrl];
-        }
+        fileToShare = [self downloadFile:fileUrl];
     } else {
-        NSURL *fileUrl = [NSURL fileURLWithPath:shareFile];
-        [self displayDocument:fileUrl];
+        fileToShare = [NSURL fileURLWithPath:shareFile];
+    }
+    
+    if (fileToShare) {
+        [self displayDocument:fileToShare isLimited:isLimited callback:callback];
     }
 }
 
-- (void) displayDocument:(NSURL*)fileUrl {
-    self.documentController = [UIDocumentInteractionController interactionControllerWithURL:fileUrl];
-    self.documentController.delegate = self;
+- (void) displayDocument:(NSURL*)fileUrl isLimited:(BOOL)isLimited callback:(RCTResponseSenderBlock)callback {
     UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    [self.documentController presentOptionsMenuFromRect:CGRectZero inView:ctrl.view animated:YES];
+    NSArray *items = @[fileUrl];
+    UIActivityViewController *activityController = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil];
+
+    if (isLimited) {
+        activityController.excludedActivityTypes = @[UIActivityTypeAirDrop,
+                                                    UIActivityTypeAddToReadingList,
+                                                    UIActivityTypeCopyToPasteboard,
+                                                    UIActivityTypeSaveToCameraRoll,
+                                                    UIActivityTypePrint,
+                                                    UIActivityTypeAssignToContact,
+                                                    UIActivityTypeCopyToPasteboard,
+                                                    @"com.apple.reminders.RemindersEditorExtension",
+                                                    @"com.apple.mobilenotes.SharingExtension",
+                                                    @"com.apple.mobileslideshow.StreamShareService"];
+    }
+    
+    // For iPad only
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        activityController.modalPresentationStyle = UIModalPresentationPopover;
+        
+        UIPopoverPresentationController *presentationController = activityController.popoverPresentationController;
+        [presentationController setDelegate:self];
+        presentationController.permittedArrowDirections = 0;
+        presentationController.sourceView = [[UIApplication sharedApplication] keyWindow];
+        presentationController.sourceRect = [[UIApplication sharedApplication] keyWindow].bounds;
+        
+        [ctrl setPreferredContentSize:CGSizeMake(320, 480)];
+    }
+    
+    [ctrl presentViewController:activityController animated:YES completion:^{
+        callback(@[[NSNull null]]);
+    }];
 }
 
 - (NSURL*) downloadFile:(NSURL *)fileUrl {
@@ -42,10 +76,8 @@ RCT_EXPORT_METHOD(open:(NSDictionary *)options :(RCTResponseSenderBlock)callback
     NSURL *tmpDir = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
     NSString  *tmpFilePath = [NSString stringWithFormat:@"%@/%@", [tmpDir path], fileName];
     NSURL *tmpFileUrl = [NSURL fileURLWithPath:tmpFilePath];
-    
     NSData *urlData = [NSData dataWithContentsOfURL:fileUrl];
     [urlData writeToFile:tmpFilePath atomically:YES];
-
     return tmpFileUrl;
 }
 
