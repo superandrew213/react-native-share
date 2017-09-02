8018478c3896e78e0dd9790559ffda78859d77aa
Andrew Shini <superandrew213@gmail.com>
2017-09-02 23:19:09 +1000

    Revert "Remove createJSModules"
    This reverts commit 79892a4533092561ac65b963e0d25163711cc0c6.

--- android/src/main/java/cl/json/RNSharePackage.java
+++ android/src/main/java/cl/json/RNSharePackage.java
@@ -16,6 +16,11 @@ public class RNSharePackage implements ReactPackage {
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
